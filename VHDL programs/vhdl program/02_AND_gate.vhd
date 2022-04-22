6b4936673dbd95544f74664ffa85d02883465d91
Khanjan Mehta <44535494+agenda021@users.noreply.github.com>
2022-04-22 18:23:45 +0530

    Add files via upload

---
+++ VHDL programs/vhdl program/02_AND_gate.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- PROGRAM: AND gate simulation
+
+-- PROGRAMMER: NEETA A PATADE.
+-- COMPANY: PROLIFIC EMBEDDED SYSTEMS.
+-- DATE: 01/07/2019.
+--------------------------------------
+		
+-- two descriptions provided
+--------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+--------------------------------------------------
+
+entity AND_ent is
+port(	x: in std_logic;
+	y: in std_logic;
+	F: out std_logic
+);
+end AND_ent;  
+
+--------------------------------------------------
+
+architecture behav1 of AND_ent is
+begin
+
+    process(x, y)
+    begin
+        -- compare to truth table
+        if ((x='1') and (y='1')) then
+	    F <= '1';
+	else
+	    F <= '0';
+	end if;
+    end process;
+
+end behav1;
+
+architecture behav2 of AND_ent is
+begin
+
+    F <= x and y;
+
+end behav2;
+
+--------------------------------------------------
---
+++ VHDL programs/vhdl program/10_dff-.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- PROGRAM:D FLIPFLOP simulation
+
+-- PROGRAMMER: NEETA A PATADE.
+-- COMPANY: PROLIFIC EMBEDDED SYSTEMS.
+-- DATE: 01/07/2019.
+-----------------------------------------------------------------------------------
+-- D Flip-Flop (ESD book Chapter 2.3.1)
+
+--
+-- Flip-flop is the basic component in 
+-- sequential logic design
+-- we assign input signal to the output 
+-- at the clock rising edge 
+---------------------------------------------
+
+library ieee ;
+use ieee.std_logic_1164.all;
+use work.all;
+
+---------------------------------------------
+
+entity dff is
+port(	data_in:	in std_logic;
+	clock:		in std_logic;
+	data_out:	out std_logic
+);
+end dff;
+
+----------------------------------------------
+
+architecture behv of dff is
+begin
+
+    process(data_in, clock)
+    begin
+
+        -- clock rising edge
+
+	if (clock='1' and clock'event) then
+	    data_out <= data_in;
+	end if;
+
+    end process;	
+
+end behv;
+
+----------------------------------------------
